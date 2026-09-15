1d63fda6a21cc68d01f9b240ab57b8a1460b0087
Yves <sevyns.pro@gmail.com>
2026-09-15 17:04:26 +0200

    Add : fn ajout M modif +position + present fini
    Signed-off-by: Yves <sevyns.pro@gmail.com>

--- src/gestionPlaylistMusicale.kt
+++ src/gestionPlaylistMusicale.kt
@@ -1,17 +1,54 @@
-fun addMusic(liste: MutableList<String>, musique: String, position: Int) {
-    val lastPosition: Int = liste.size;
+import kotlin.collections.mutableListOf
 
-    if (position == 0) {
-        liste.add(0, musique)
+data class Playlist(val music: MutableList<String> = mutableListOf() ) {
+
+    fun addMusic(musique: String, position: Int) {
+        val lastPosition: Int = music.size;
+
+        if (position == 0) {
+            music.add(0, musique)
+        }
+        if (position == 1) {
+            music.add(musique)
+        }
     }
-    if (position == 1) {
-        liste.add(musique)
+
+    fun modifMusic(oldMusic: String, newMusic: String){
+        val indexOldMusic: Int = music.indexOf(oldMusic);
+        music[indexOldMusic] = newMusic
+    }
+
+    fun isHePresent(musics: String): Boolean = if (musics in music) { true } else {false}
+
+    fun positionOfMusic(musique: String): Int {
+        return music.indexOf(musique)
+    }
+
+    fun delectMusic(musics: String): mutableListOf() {
+        return music.drop(positionOfMusic(musics))
     }
 }
+/*
+fun isHePresent(playlist: MutableList<String>, music: String): Boolean = if (music in playlist) { true } else {false}*/
 
 fun main() {
-    var playlist = mutableListOf("Bohemian Rhapsody", "Stairway to Heaven", "Hotel California");
-    addMusic(playlist, "Imagine", 1);
-    addMusic(playlist, "Like a Rolling Stone", 0)
-    println(playlist);
+    var p = mutableListOf("Bohemian Rhapsody", "Stairway to Heaven", "Hotel California");
+    val maPlaylist = Playlist(p);
+
+    maPlaylist.addMusic("Imagine", 1);
+    maPlaylist.addMusic("Like a Rolling Stone", 0);
+    println(maPlaylist)
+    maPlaylist.addMusic("Bohemian Rhapsody", 1);
+    println(maPlaylist);
+    maPlaylist.modifMusic("Stairway to Heaven", "Smells Like Teen Spirit")
+    println(maPlaylist);
+    println(maPlaylist.isHePresent("Hotel California"));
+    println("Imagine apparait à la position ${maPlaylist.positionOfMusic("Imagine")}");
+    maPlaylist.delectMusic("Hotel California")
+    println(maPlaylist);
+    /*
+
+    println(isHePresent(playlist,"Hotel California"))
+    println("${positionOfMusic(playlist, "Imagine")}")*/
+
 }
